353eec4e241b9a20083f364a62bfcc5d71dd6858
unknown <sidiwhynot@gmail.com>
2024-06-12 03:58:31 +0200

    footer mise a jour

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BottomNavigationBarComponent } from '../../bottom-navigation-bar/bottom-navigation-bar.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ BottomNavigationBarComponent,TranslateModule],
+  imports: [ BottomNavigationBarComponent,TranslateModule,RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
